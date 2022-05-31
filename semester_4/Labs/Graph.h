41ef3c3c4b949601633069f40e6cda8a18b93453
gwymlas <smrszr63@gmail.com>
2022-05-31 16:27:46 +0400

    lab_3 is completed

--- semester_4/Labs/Graph.h
+++ semester_4/Labs/Graph.h
@@ -1,11 +1,134 @@
 #ifndef ADS_LABS_2C2S_GRAPH_H
 #define ADS_LABS_2C2S_GRAPH_H
+#include <utility>
 #include <vector>
+#include <string>
 #include <list>
 #include <queue>
 #include <map>
 #include <exception>
 #include <iostream>
+
+
+class Town {
+    std::string _name;
+    size_t _population;
+
+public:
+    Town(): _name(std::string()), _population(0) { }
+
+    Town(const char* name, const size_t population): _name(name), _population(population) { }
+
+    Town(const Town& rhs) {
+        _name = rhs._name;
+        _population = rhs._population;
+    }
+
+    bool operator== (const Town& rhs) const {
+        return _name == rhs._name;
+    }
+
+
+    std::string get_name() const {
+        return _name;
+    }
+
+    size_t get_population() const {
+        return _population;
+    }
+
+    void print_town() const {
+        std::cout << "Name of town: " << _name << "\n";
+        std::cout << "Population of town: " << _population << "\n";
+    }
+};
+
+//std::ostream& operator<< (std::ostream& out, const Town& rhs) {
+//    out << "Name of town: " << rhs.get_name() << "\n";
+//    out << "Population of town: " << rhs.get_population() << "\n";
+//    return out;
+//}
+
+enum type_road {
+    federal = 0,
+    regional,
+    local,
+    private_road,
+};
+
+class Road {
+
+    std::string _name;
+    type_road _type;
+    double _length;
+    double _price;
+
+public:
+    Road(): _name(std::string()), _type(local), _length(0), _price(0) { }
+
+    Road(const char* name, const type_road type, const double length, const double price): _name(name), _type(type),
+                                                                                           _length(length), _price(price) { }
+
+    Road(const Road& rhs) {
+        _name = rhs._name;
+        _type = rhs._type;
+        _length = rhs._length;
+        _price = rhs._price;
+    }
+
+    std::string get_name() const {
+        return _name;
+    }
+
+    type_road get_type() const {
+        return _type;
+    }
+
+    double get_length() const {
+        return _length;
+    }
+
+    double get_price() const {
+        return _price;
+    }
+
+    explicit operator double() const {
+        double result = 0;
+        switch (_type) {
+            case type_road::federal:
+                result += _length;
+                break;
+            case type_road::regional:
+                result += _length * 1;
+                break;
+            case type_road::local:
+                result += _length * 1;
+                break;
+            case type_road::private_road:
+                result += _length * 1;
+                break;
+            default:
+                throw std::logic_error("Invalid roadType enum value");
+        }
+        //result += _price * _length;
+        return result;
+    }
+
+};
+
+//std::ostream& operator<< (std::ostream& out, const Road& rhs) {
+//    out << "Name of road: " << rhs.get_name() << "\n";
+//    out << "Type of road: " << rhs.get_type() << "\n";
+//    out << "Lenght of road: " << rhs.get_length() << "\n";
+//    return out;
+//}
+
+template<typename TRoad>
+struct defaultWeightConverter {
+    double operator()(const TRoad& road) const {
+        return static_cast<double>(road);
+    }
+};
 
 
 template<typename TVertex, typename TEdge>
@@ -100,13 +223,13 @@ public:
     void print() {
         for (size_t i = 0; i < table.size(); ++i) {
             std::cout << "information about the town: " << std::endl;
-            std::cout << "Name: " << table[i]._data << std::endl;
+            table[i]._data.print_town();
             std::cout << "The roads from this town:" << std::endl;
             if (table[i]._edges.begin() == table[i]._edges.end()) {
                 std::cout << "There are no roads" << std::endl;
             }
             for (auto elem: table[i]._edges) {
-                std::cout << "\t" << "Destination: " << elem._dst << "---" << "Length: " << elem._data << std::endl;
+                std::cout << "\t" << "Name of road: " << elem._data.get_name() << "  Length: " << elem._data.get_length() << std::endl;
             }
             std::cout<<std::endl;
         }
@@ -131,7 +254,7 @@ public:
                     q.push(v);
                 }
             }
-            std::cout << u._data << std::endl;
+            u._data.print_town();
         }
     }
 
@@ -143,7 +266,7 @@ public:
         std::vector<int> prev(table.size(), -1);
         distance[index_src] = 0;
 
-        for (size_t i = 0; i + 1 < table.size(); ++i) {
+        for (size_t i = 0; i < table.size() - 1; ++i) {
             for(size_t j = 0; j < table.size(); ++j) {
                 for (auto elem: table[j]._edges) {
                     int id_dst = get_id(elem._dst);
@@ -159,11 +282,13 @@ public:
             for (auto elem: table[j]._edges) {
                 int id_dst = get_id(elem._dst);
                 if (distance[id_dst] > distance[j] + static_cast<double>(elem._data)) throw std::exception();
+                //std::cout << "!!!!" << distance[id_dst] << " " << distance[j] << "  " << static_cast<double>(elem._data) << std::endl;
             }
         }
 
         std::list<TVertex> path;
         for (int i = index_dst; i != -1; i = prev[i]) {
+            if (distance[i] == std::numeric_limits<double>::max()) throw std::exception();
             path.push_front(table[i]._data);
         }
         return make_pair(path, distance[index_dst]);
--- semester_4/Labs/main.cpp
+++ semester_4/Labs/main.cpp
@@ -3,36 +3,64 @@
 #include <string>
 
 int main() {
-    Graph<std::string, double> g;
-    g.add_vertex("Samara");
-    g.add_vertex("Moscow");
-    g.add_vertex("Piter");
-    g.add_vertex("Orenburg");
-    g.add_vertex("Syzran");
-    g.add_vertex("TLT");
-    g.add_vertex("Kirov");
-    g.add_edge("Samara", "Piter", 10);
-    g.add_edge("Samara", "Moscow", 6);
-    g.add_edge("Moscow", "Piter", 2);
-    g.add_edge("Samara", "Orenburg", 11);
-    g.add_edge("Piter", "Orenburg", 22);
-    g.add_edge("Piter", "Syzran", 224);
-    g.add_edge("Piter", "TLT", 242);
-    g.add_edge("Piter", "Kirov", 232);
-    auto result = g.bellman_ford_algorithm("Samara", "Kirov");
-    std::cout << "Кратчайший путь: " << std::endl;
-    for (auto elem: result.first) {
-        std::cout << elem << "->";
-    }
-    std::cout << result.second << std::endl;
-    std::cout << "------------------" << std::endl;
+    auto Samara = Town("Samara", 1100);
+    auto Moscow = Town("Moscow", 15000);
+    auto Piter = Town("Piter", 8000);
+    auto Orenburg = Town("Orenburg", 800);
+    auto Syzran = Town("Syzran", 180);
+    auto TLT = Town("TLT", 700);
+    auto Kirov = Town("Kirov", 900);
+
+    auto Samara_Piter = Road("M1", federal, 20, 1.2 );
+    auto Samara_Moscow = Road("M2", federal, 10, 1.3 );
+    auto Moscow_Piter = Road("M3", federal, 12, 0 );
+    auto Samara_Orenburg = Road("M4", federal, 9, 0 );
+    auto Piter_Orenburg = Road("M5", federal, 7, 0 );
+    auto Piter_Syzran = Road("M6", federal, 45, 1 );
+    auto Piter_TLT = Road("M7", federal, 22, 0 );
+    auto Piter_Kirov = Road("M8", federal, 11, 0 );
+
+    Graph<Town, Road> g;
+
+    g.add_vertex(Samara);
+    g.add_vertex(Moscow);
+    g.add_vertex(Piter);
+    g.add_vertex(Orenburg);
+    g.add_vertex(Syzran);
+    g.add_vertex(TLT);
+    g.add_vertex(Kirov);
+    g.add_edge(Samara, Piter, Samara_Piter);
+    g.add_edge(Samara, Moscow, Samara_Moscow);
+    g.add_edge(Moscow, Piter, Moscow_Piter);
+    g.add_edge(Samara, Orenburg,Samara_Orenburg);
+    g.add_edge(Piter, Orenburg, Piter_Orenburg);
+    g.add_edge(Piter, Syzran, Piter_Syzran);
+    g.add_edge(Piter, TLT, Piter_TLT);
+    g.add_edge(Piter, Kirov, Piter_Kirov);
+
     g.print();
-    std::cout << "------------------" << std::endl;
-    g.BFS("Piter");
-    g.delete_edge("Samara", "Piter");
-    //g.print();
-    g.delete_vertex("Moscow");
-    //g.print();
+    auto result = g.bellman_ford_algorithm(Samara, Kirov);
+    std::cout << "Кратчайший путь: " << std::endl;
+    for (const auto& elem: result.first) {
+        elem.print_town();
+        std::cout << "->" << std::endl;
+    }
+    std::cout << "Итоговый вес пути: " << result.second << std::endl;
+
+//    g.delete_vertex(Moscow);
+//    g.print();
+//    result = g.bellman_ford_algorithm(Samara, Kirov);
+//    std::cout << "Кратчайший путь: " << std::endl;
+//    for (const auto& elem: result.first) {
+//        elem.print_town();
+//        std::cout << "->";
+//    }
+//    std::cout << result.second << std::endl;
+
+    std::cout << "\n" <<"Обход в ширину" << std::endl;
+    g.BFS(Piter);
+
+
 
     return 0;
 }
